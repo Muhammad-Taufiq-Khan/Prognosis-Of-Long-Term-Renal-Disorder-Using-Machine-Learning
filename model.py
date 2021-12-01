29084b7b68027a9e791de9b9d37f923784e8426f
Md Taufiqul Haque Khan Tusar <taufiqtusar@gmail.com>
2021-12-02 01:17:08 +0600

    flaskCKD App Pushed
    Uploaded Directory: Desktop/flaskCKD  
    Ignored files: __pychache__, Pipfile, Pipfile.lock

---
+++ model.py
@@ -0,0 +1,106 @@
+                     # Dependencies list to execute programs
+
+#Data manipulation
+import numpy as np
+# from numpy.core.numeric import NaN
+import pandas as pd
+
+#Extra
+# import math
+import warnings
+warnings.filterwarnings(action='ignore')
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import MinMaxScaler
+normalizer = MinMaxScaler()
+
+from sklearn.impute import KNNImputer #import Knn Imputer
+imputer = KNNImputer(n_neighbors = 5) #Declaring that 5 nearest neighbour should be decided
+
+from sklearn.neighbors import LocalOutlierFactor
+lof = LocalOutlierFactor(n_neighbors=15,novelty=False) # identify outliers in the training dataset
+
+from imblearn.over_sampling import SMOTE
+oversample = SMOTE()
+
+from sklearn.preprocessing import StandardScaler
+standardization  = StandardScaler()
+
+# from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_auc_score
+
+
+def CKD_Prediction(specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias):
+
+    #Fetch Data from my GitHub Repo
+    url= "https://raw.githubusercontent.com/Muhammad-Taufiq-Khan/Prognosis-Of-Long-Term-Renal-Disorder-Using-Machine-Learning/main/FlaskCKD_CleanData.csv"
+    dataframe = pd.read_csv(url)
+
+    """ # Spliting Data Set"""    
+    Xtrain, X_test, ytrain, y_test = train_test_split(dataframe.drop('ckd',axis=1), dataframe["ckd"], test_size = 0.25, random_state=40)
+    
+
+
+    """DATA ENGINEERING & MACHINE LEARNING TASKS START HERE"""
+    #09 Important Feature
+    Xtrain = Xtrain[['specific_gravety', 'albumin','blood _urea', 'serum_creatinine', 'hemoglobine', 'red_blood_cells', 'hypertension', 'diabetes_mellitias']]
+
+
+
+    """NORMALIZATION MIN-MAX NORMALIZER"""
+    #Normalizing Training Data
+    Xtrain = normalizer.fit_transform(Xtrain)
+
+
+
+    """K-NN Imputation on Training Dataset"""
+    imputer.fit(Xtrain) #fitting imputer according to Training Features (X_train)
+    Xtrain = imputer.transform(Xtrain) #Appling Imputation
+
+
+
+    """OUTLIER DETECTION & REMOVAL: LOCAL OUTLIER FACTOR (LOF)"""
+    # lof.fit(Xtrain)
+    yhat = lof.fit_predict(Xtrain)
+    # select all rows that are not outliers
+    mask = yhat != -1
+    Xtrain, ytrain = Xtrain[mask, :], ytrain[mask]
+
+
+
+    """OVERSAMPLING: SYNTHETIC MINORITY OVERSAMPLING TECHNIQUE"""
+    Xtrain, ytrain = oversample.fit_resample(Xtrain, ytrain)
+
+
+
+    """Standard Scalling OR Z-SCORE NORMALIZATION"""
+    Xtrain = standardization.fit_transform(Xtrain)
+
+
+    """MODEL TRAINING: RANDOM FOREST"""
+    from sklearn.ensemble import RandomForestClassifier
+    RFmodel = RandomForestClassifier(n_estimators=1000).fit(Xtrain, ytrain)
+
+
+
+    """PREDICTION"""
+    #Using Numpy Array
+    Xtest = np.array([[specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias]])
+    # Xtest = Xtest.reshape((1,8))
+
+    # # Using List
+    # Xtest = [[specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias]]
+    
+    #Previous Code
+    # Xtest = Xtest[['specific_gravety', 'albumin','blood _urea', 'serum_creatinine', 'hemoglobine', 'red_blood_cells','pus_cell_clumps', 'hypertension', 'diabetes_mellitias']]
+    #Normalizing Testing Data
+    Xtest = normalizer.transform(Xtest)
+    Xtest = imputer.transform(Xtest) #Appling Imputation
+    Xtest = standardization.transform(Xtest)
+    
+    return RFmodel.predict(Xtest)
+
+# CKD_Prediction(specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias)
+# CKD_Prediction(1.005, 4.0, 11.2, 32.0, 1.0, 0.0, 1.0, 0.0)
+# CKD_Prediction(NaN, 1.0, NaN, 30.0,NaN, 0, 1.0, NaN)
+# CKD_Prediction(1.025, 0, 14.5, 52, NaN, 0, 0, 0)
